30d5e8ebe62cb5a053b541b0f37e6d38e0e02565
mendesmoe <m.mendes@depiltech.com>
2024-03-18 17:26:03 +0100

    Refacto: FeedBack - tests repository, gateway and controller

--- src/test/java/com/postech/tabletrust/gateway/FeedBackGatewayTest.java
+++ src/test/java/com/postech/tabletrust/gateway/FeedBackGatewayTest.java
@@ -1,12 +1,9 @@
 package com.postech.tabletrust.gateway;
 
 import com.postech.tabletrust.entity.FeedBack;
-import com.postech.tabletrust.entity.Reservation;
 import com.postech.tabletrust.entity.Restaurant;
-import com.postech.tabletrust.exception.InvalidReservationException;
 import com.postech.tabletrust.gateways.FeedBackGateway;
 import com.postech.tabletrust.repository.FeedBackRepository;
-import com.postech.tabletrust.repository.ReservationRepository;
 import com.postech.tabletrust.repository.RestaurantRepository;
 import com.postech.tabletrust.utils.NewEntititesHelper;
 import org.junit.jupiter.api.AfterEach;
@@ -20,15 +17,12 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
@@ -36,16 +30,16 @@ public class FeedBackGatewayTest {
     @Mock
     private FeedBackRepository feedBackRepository;
     @Mock
-    private ReservationRepository reservationRepository;
-    @Mock
     private RestaurantRepository restaurantRepository;
 
     private FeedBackGateway feedBackGateway;
+
     AutoCloseable openMocks;
 
     @BeforeEach
     void setup(){
         openMocks = MockitoAnnotations.openMocks(this);
+        feedBackGateway = new FeedBackGateway(feedBackRepository, restaurantRepository);
     }
 
     @AfterEach
@@ -56,7 +50,7 @@ public class FeedBackGatewayTest {
     @Nested
     class ListFeedbacks {
         @Test
-        void shouldListAllFeedBacksByARestaurant(){
+        void shouldListAllFeedBacksByARestaurantPageable(){
             Restaurant restaurant = NewEntititesHelper.createARestaurant();
             FeedBack fb = NewEntititesHelper.createAFeedBack();
             Pageable pageable = PageRequest.of(0,10);
@@ -66,14 +60,15 @@ public class FeedBackGatewayTest {
 
             // Configuração dos mocks para o repositório
             when(restaurantRepository.findById(restaurant.getId())).thenReturn(Optional.of(restaurant));
-            when(feedBackRepository.findByRestaurantId(restaurant.getId(), pageable)).thenReturn(fbPage);
+            when(feedBackRepository.findByRestaurantId(restaurant.getId(),pageable)).thenReturn(fbPage);
 
-            var listOfFB = feedBackGateway.listFeedBackByRestaurantId(restaurant.getId());
+            //Assert
+            var listOfFB = feedBackGateway.feedBackByRestaurantIdPageable(pageable, restaurant.getId());
 
             assertThat(listOfFB).contains(fb);
         }
 
-        @Test
+        @Test //OK
         void shouldFindByID(){
             //Assert
             var fb = NewEntititesHelper.createAFeedBack();
@@ -91,63 +86,19 @@ public class FeedBackGatewayTest {
         }
     }
 
-    @Nested
+    @Nested //OK
     class CreateFeedbacks{
-        @Test
+        @Test //OK
         void shouldCreateANewFeedBack() throws Exception {
-            Reservation reservation = NewEntititesHelper.createAReservation();
             FeedBack feedBack = NewEntititesHelper.createAFeedBack();
 
-            when(reservationRepository.findById(any(UUID.class))).thenReturn(Optional.of(reservation)); //Mock le findById
-            when(feedBackRepository.save(any(FeedBack.class))).thenAnswer( i -> i.getArgument(0)); // Mock o save
+          when(feedBackGateway.createFeedback(any(FeedBack.class))).thenAnswer( i -> i.getArgument(0)); // Mock o save
 
             //Act
             var feedback = feedBackGateway.createFeedback(feedBack);
 
             //Assert
             assertThat(feedback).isNotNull().isInstanceOf(FeedBack.class);
-        }
-
-        @Test
-        void shouldReturnAnExceptionNotFoundReservation() throws Exception {
-            Reservation reservation = NewEntititesHelper.createAReservation();
-            reservation.setId(UUID.randomUUID()); //change id
-            FeedBack feedBack = NewEntititesHelper.createAFeedBack();
-
-            when(reservationRepository.findById(any(UUID.class))).thenReturn(Optional.empty()); //Not found
-
-            assertThatThrownBy(() -> feedBackGateway.createFeedback(feedBack))
-                    .isInstanceOf(InvalidReservationException.class)
-                    .hasMessage("A reserva informada nao foi encontrada");
-        }
-
-        @Test
-        void shouldReturnAnExceptionForReservationNotApprouved() throws Exception {
-            Reservation reservation = NewEntititesHelper.createAReservation();
-            reservation.setApproved(false); // Change 'approved'
-            FeedBack feedBack = NewEntititesHelper.createAFeedBack();
-
-            when(reservationRepository.findById(any(UUID.class))).thenReturn(Optional.of(reservation)); //OK
-
-            assertThatThrownBy(() -> feedBackGateway.createFeedback(feedBack))
-                    .isInstanceOf(InvalidReservationException.class)
-                    .hasMessage("A reserva informada foi anulada ou ainda nao terminou");
-        }
-
-        @Test
-        void shouldReturnAnExceptionForReservationInThreeHours() throws Exception {
-            Reservation reservation = NewEntititesHelper.createAReservation();
-
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
-            LocalDateTime less1Hour = LocalDateTime.now().minusHours(1);
-            reservation.setReservationDate(less1Hour); // Change hour by reservation - invalid
-            FeedBack feedBack = NewEntititesHelper.createAFeedBack();
-
-            when(reservationRepository.findById(any(UUID.class))).thenReturn(Optional.of(reservation)); //OK
-
-            assertThatThrownBy(() -> feedBackGateway.createFeedback(feedBack))
-                    .isInstanceOf(InvalidReservationException.class)
-                    .hasMessage("A reserva informada foi anulada ou ainda nao terminou");
         }
     }
 
@@ -156,14 +107,14 @@ public class FeedBackGatewayTest {
         @Test
         void shouldDeleteByID(){
             //Assert
-            var fb = NewEntititesHelper.createAFeedBack();
-            UUID id = fb.getId();
+            var feedback = NewEntititesHelper.createAFeedBack();
+            UUID id = feedback.getId();
 
-            when(feedBackRepository.findById(id)).thenReturn(Optional.of(fb));
+            when(feedBackRepository.findById(id)).thenReturn(Optional.of(feedback));
             doNothing().when(feedBackRepository).deleteById(id);
 
-            var removed = feedBackGateway.deleteById(id);
-            assertThat(removed).isTrue();
+            var fbRemoved = feedBackGateway.deleteById(id);
+            assertThat(fbRemoved).isTrue();
 
             verify(feedBackRepository, times(1)).deleteById(any(UUID.class));
         }
